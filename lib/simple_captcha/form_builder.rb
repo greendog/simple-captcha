4ff33a74eda8d4c44d2a1326e1b217f1e7ade389
Azdaroth <azdaroth@gmail.com>
2013-05-23 10:28:56 +0200

    beack to previous version with rails sprockets

--- lib/simple_captcha/form_builder.rb
+++ lib/simple_captcha/form_builder.rb
@@ -4,8 +4,8 @@ module SimpleCaptcha
       base.send(:include, SimpleCaptcha::ViewHelper)
       base.send(:include, SimpleCaptcha::FormBuilder::ClassMethods)
       base.send(:include, ActionView::Helpers)
-      base.send(:include, Sprockets::Rails::Helper::RailsHelper)
-      base.send(:include, Sprockets::Rails::Helper::IsolatedHelper)
+      base.send(:include, Sprockets::Rails::Helpers::RailsHelper)
+      base.send(:include, Sprockets::Rails::Helpers::IsolatedHelper)
       
       base.delegate :render, :session, :to => :template
     end
